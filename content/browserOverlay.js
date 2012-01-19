a25d41901625db30fee5c2c92ae04e7c0bab0b5b
Rohan Chitambar <rohan.a.chitambar@gmail.com>
2012-01-20 04:49:56 +0530

    Getting and Updating extio with remote content

--- content/browserOverlay.js
+++ content/browserOverlay.js
@@ -19,3 +19,23 @@ StockQuoteMonitor.BrowserOverlay = {
     window.alert(message);
   }
 };
+
+
+StockQuoteMonitor.Service = {
+  /**
+  * Get The Data From The Remote Service
+  */
+  getQuoteData : function() {
+	var url = "http://localhost:8080/StockQuoteServlet";
+	var request = Components.classes["@mozilla.org/xmlextras/xmlhttprequest;1"]  
+				.createInstance(Components.interfaces.nsIXMLHttpRequest);
+	request.onload = function(aEvent) {  
+	   window.alert("Response Text: " + aEvent.target.responseText);  
+	};  
+	request.onerror = function(aEvent) {  
+	   window.alert("Error Status: " + aEvent.target.status);  
+	};  
+	request.open("GET", url, true);  
+	request.send(null);
+  }
+};
